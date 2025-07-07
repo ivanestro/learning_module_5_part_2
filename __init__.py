25c53347eae1978a363bce7769790fcf20759cbc
ivanestro <iestropigan@rrc.ca>
2025-07-07 10:15:17 -0500

    Created first unit test

---
+++ __init__.py
@@ -0,0 +1,2 @@
+def greet_name_age(name: str, age: int) -> str:
+    return f"Hello {name}, you are {age} years old!"
---
+++ tests/test_functions.py
@@ -0,0 +1,42 @@
+"""
+Description: Module 05 demonstration: Functions with Unit Testing
+Author: Ivan Estropigan
+Date: 2025, 07,07 
+Usage: To execute the unit tests: 
+        From the unit_testing directory in the Terminal:
+        python -m unittest -v tests/test_functions.py
+    To execute the python src program:
+        From the unit_testing directory in the Terminal:
+        python src/functions.py
+"""
+
+import unittest
+from unittest.mock import patch
+from src.functions import greet_name_age, grade_outcome
+
+
+class TestFunctions(unittest.TestCase):
+    def test_greet_name_with_all_parameters(self):
+        # Arrange variables
+        name = "Joe"
+        age = 25
+        expected = "Hello Joe, you are 25 years old!"
+        # note: spelled Joe incorrectly to show failed test.
+
+        # Act running the code what we're testing
+        actual = greet_name_age(name, age)
+
+        # Verify the output (Assert):
+        # Assert compares two values 
+        self.assertEqual(expected, actual)
+
+    def test_grade_outcome_a_plus(self):
+        # Arrange
+        grade = 91
+        expected = "A+"
+
+        # Act 
+        actual = grade_outcome(grade)
+
+        # Assert 
+        self.assertEqual(expected, actual)
